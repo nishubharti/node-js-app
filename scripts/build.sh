5cefeecafd608df9ffcbd3712c747fa82674ec68
nishubharti <32607124+nishubharti@users.noreply.github.com>
2022-09-02 17:44:33 +0530

    save artifactory (#38)
    Co-authored-by: Nishu.Bharti1 <Nishu.Bharti1@ibm.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,3 +5,21 @@ set -euo pipefail
 # shellcheck disable=SC2086
 docker build $DOCKER_BUILD_ARGS .
 docker push "${IMAGE}"
+
+DIGEST="$(docker inspect --format='{{index .RepoDigests 0}}' "${IMAGE}" | awk -F@ '{print $2}')"
+
+#
+# Save the artifact to the pipeline, 
+# so it can be scanned and signed later
+#
+save_artifact app-image \
+    type=image \
+    "name=${IMAGE}" \
+    "digest=${DIGEST}" \
+    "tags=${IMAGE_TAG}"
+
+url="$(load_repo app-repo url)"
+sha="$(load_repo app-repo commit)"
+
+save_artifact app-image \
+"source=${url}.git#${sha}"
