0b315ac16ff021170f0acd47c66bd29b385fd4cd
nishubharti <32607124+nishubharti@users.noreply.github.com>
2022-09-03 21:02:54 +0530

    uplpad file to artifactory (#45)
    Co-authored-by: Nishu.Bharti1 <Nishu.Bharti1@ibm.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,37 +1,100 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -x
+#
+# prepare data for the release step. Here we upload all the metadata to the Inventory Repo.
+# If you want to add any information or artifact to the inventory repo then use the "cocoa inventory add command"
+#
 
-echo "Inventory Add .....";
-echo "GIT_COMMIT=${GIT_COMMIT}"
-echo "GHE_TOKEN=${GHE_TOKEN}"
-echo "GHE_ORG=${GHE_ORG}"
-echo "GHE_REPO=${GHE_REPO}"
-echo "SIGNATURE=${SIGNATURE}"
-echo "BUILD_NUMBER=${BUILD_NUMBER}"
-echo "PIPELINE_RUN_ID=${PIPELINE_RUN_ID}"
-echo "APP_REPO_NAME=${APP_REPO_NAME}"
-echo "APP_REPO=${APP_REPO}"
-echo "ARTIFACT_PROVENANCE=${ARTIFACT_PROVENANCE}"
-echo "SHA_VAL=${SHA_VAL}"
-echo "VERSION=${VERSION}"
-echo "TYPE=${TYPE}"
-echo "BUILD_IMAGE_TAG=${BUILD_IMAGE_TAG}"
-echo "IMAGE_NAME=${IMAGE_NAME}"
-echo "COMMIT_BRANCH=${COMMIT_BRANCH}"
+export GHE_TOKEN="$(cat /config/git-token)"
 
-APP_ARTIFACTS='{ "signature": "'${SIGNATURE}'", "provenance": "'${ARTIFACT_PROVENANCE}'", "image_tag": "'${BUILD_IMAGE_TAG}'", "image_name": "'${IMAGE_NAME}'", "branch": "'${COMMIT_BRANCH}'" }'
+# shellcheck source=/dev/null
+. "${ONE_PIPELINE_PATH}/tools/get_repo_params"
 
-cocoa inventory add \
-    --artifact="${ARTIFACT_PROVENANCE}" \
-    --repository-url="${APP_REPO}" \
-    --commit-sha="${GIT_COMMIT}" \
-    --build-number="${BUILD_NUMBER}" \
-    --pipeline-run-id="${PIPELINE_RUN_ID}" \
-    --version="${VERSION}" \
-    --name="${APP_REPO_NAME}" \
-    --app-artifacts="${APP_ARTIFACTS}" \
-    --sha256="${SHA_VAL}" \
-    --provenance="${ARTIFACT_PROVENANCE}" \
-    --signature="${SIGNATURE}" \
-    --type="${TYPE}"
+APP_REPO="$(load_repo app-repo url)"
+APP_REPO_ORG=${APP_REPO%/*}
+APP_REPO_ORG=${APP_REPO_ORG##*/}
+APP_REPO_NAME=${APP_REPO##*/}
+APP_REPO_NAME=${APP_REPO_NAME%.git}
+
+COMMIT_SHA="$(load_repo app-repo commit)"
+
+APP_ABSOLUTE_SCM_TYPE=$(get_absolute_scm_type "$APP_REPO")
+
+INVENTORY_TOKEN_PATH="./inventory-token"
+read -r INVENTORY_REPO_NAME INVENTORY_REPO_OWNER INVENTORY_SCM_TYPE INVENTORY_API_URL < <(get_repo_params "$(get_env INVENTORY_URL)" "$INVENTORY_TOKEN_PATH")
+#
+# collect common parameters into an array
+#
+params=(
+    --repository-url="${APP_REPO}"
+    --commit-sha="${COMMIT_SHA}"
+    --version="${COMMIT_SHA}"
+    --build-number="${BUILD_NUMBER}"
+    --pipeline-run-id="${PIPELINE_RUN_ID}"
+    --org="$INVENTORY_REPO_OWNER"
+    --repo="$INVENTORY_REPO_NAME"
+    --git-provider="$INVENTORY_SCM_TYPE"
+    --git-token-path="$INVENTORY_TOKEN_PATH"
+    --git-api-url="$INVENTORY_API_URL"
+)
+
+#
+# add the deployment file as a build artifact to the inventory
+#
+APP_TOKEN_PATH="./app-token"
+read -r APP_REPO_NAME APP_REPO_OWNER APP_SCM_TYPE APP_API_URL < <(get_repo_params "$(get_env APP_REPO)" "$APP_TOKEN_PATH")
+token=$(cat $APP_TOKEN_PATH)
+
+function upload_deployment_files_artifacts() {
+
+    deployment_file=$1
+    deployment_type=$2
+    if [ "$APP_ABSOLUTE_SCM_TYPE" == "hostedgit" ]; then
+        id=$(curl --header "PRIVATE-TOKEN: ${token}" ${APP_API_URL}/projects/${APP_REPO_ORG}%2F${APP_REPO_NAME} | jq .id)
+        DEPLOYMENT_ARTIFACT="${APP_API_URL}/projects/${id}/repository/files/${deployment_file}/raw?ref=${COMMIT_SHA}"
+    elif [ "$APP_ABSOLUTE_SCM_TYPE" == "github_integrated" ]; then
+        DEPLOYMENT_ARTIFACT="https://raw.github.ibm.com/${APP_REPO_ORG}/${APP_REPO_NAME}/${COMMIT_SHA}/${deployment_file}"
+    else
+        DEPLOYMENT_ARTIFACT="https://raw.githubusercontent.com/${APP_REPO_ORG}/${APP_REPO_NAME}/${COMMIT_SHA}/${deployment_file}"
+    fi
+    DEPLOYMENT_ARTIFACT_PATH="$(load_repo app-repo path)"
+    DEPLOYMENT_ARTIFACT_DIGEST="$(shasum -a256 "${WORKSPACE}/${DEPLOYMENT_ARTIFACT_PATH}/${deployment_file}" | awk '{print $1}')"
+
+    cocoa inventory add \
+        --artifact="${DEPLOYMENT_ARTIFACT}" \
+        --type="deployment" \
+        --sha256="${DEPLOYMENT_ARTIFACT_DIGEST}" \
+        --signature="${DEPLOYMENT_ARTIFACT_DIGEST}" \
+        --name="${APP_REPO_NAME}_${deployment_type}_deployment" \
+        "${params[@]}"
+
+}
+upload_deployment_files_artifacts deployment_iks.yml IKS
+upload_deployment_files_artifacts deployment_os.yml OPENSHIFT
+
+
+while read -r artifact; do
+    image="$(load_artifact "${artifact}" name)"
+    signature="$(load_artifact "${artifact}" signature)"
+    digest="$(load_artifact "${artifact}" digest)"
+    tags="$(load_artifact "${artifact}" tags)"
+
+    APP_NAME="$(get_env app-name)"
+    APP_ARTIFACTS='{ "app": "'${APP_NAME}'", "tags": "'${tags}'" }'
+
+    echo "image is" ${image}
+    echo "signature is" ${signature}
+    echo "digest is" ${digest}
+    echo "tags is" ${tags}
+    echo "app artifacts is" ${APP_ARTIFACTS}
+
+    cocoa inventory add \
+        --artifact="${image}@${digest}" \
+        --name="${APP_REPO_NAME}" \
+        --app-artifacts="${APP_ARTIFACTS}" \
+        --signature="${signature}" \
+        --provenance="${image}@${digest}" \
+        --sha256="${digest}" \
+        --type="image" \
+        "${params[@]}"
+done < <(list_artifacts)
